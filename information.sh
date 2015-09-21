c71751d29b2f6a4945b8e0b3d68bc4d53381a602
guomaoqiu <guomaoqiu@sina.com>
2015-09-21 22:46:46 +0800

    add information.sh

---
+++ information.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "\n"
+echo -e "姓名：\033[40;37mMAOQIU.GUO\033[0m\n" 
+echo -e "年龄: \033[41;37m24\033[0m\n" 
+echo -e "地址: \033[42;37mLiangshan Prefecture, Sichuan Province, China\033[0m\n" 
+echo -e "Email:\033[43;37mguomaoqiu@sina.com\033[0m\n" 
+echo -e "电话：\033[44;37m+86 186-8341-9650\033[0m\n" 
